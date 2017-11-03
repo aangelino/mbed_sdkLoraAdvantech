efb302b8479452e71011886c56e5e0932e1b6cb0
aangelino <33348420+aangelino@users.noreply.github.com>
2017-11-03 17:42:03 +0100

    upload DebouncedInterrupt LoraNodeLib main

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "mbed.h"
+#include "DebouncedInterrupt.h"
+
+DigitalOut led1(LED1);
+//InterruptIn mybutton(USER_BUTTON);
+
+Serial pc(SERIAL_TX, SERIAL_RX);
+
+// Interrupt Service Routine on button press.
+void button_push_isr( void )
+{
+    pc.printf("\n\r button interrupt \n\r");
+}
+
+
+// main() runs in its own thread in the OS
+int main() {
+
+pc.printf("\n\r new run 3but \n\r");
+
+/*debounce code*/
+DebouncedInterrupt user_interrupt(PA_9);
+user_interrupt.attach(button_push_isr, IRQ_RISE, 500, true);
+
+    while (true) {
+        led1 = !led1;
+        pc.printf("\n\r toggle led \n\r");
+        wait(0.5);
+    }
+}
+
